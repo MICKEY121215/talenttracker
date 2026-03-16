cc7af49a62c0cbe9ba35ca63f80efb6b498938fc
MICKEY121215 <shamitvilekar@gmail.com>
2026-03-16 06:04:57 +0530

    Week 4: Status update, dashboard and ATS finalization

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def extract_skills(text):
     clean_text = re.sub(r'[^\w\s]', ' ', text.lower())
     words = set(clean_text.split())
 
-    found_skills = []
+    found_skills = words & SKILL_SET
 
     for skill in SKILL_SET:
         if skill in clean_text:
